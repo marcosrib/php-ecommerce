ec122d07c139bc739f558359806f431b5cd1179a
marcos <marcos.rib.sousa@gmail.com>
2021-04-04 14:27:49 -0300

    add template html

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ require_once("vendor/autoload.php");
 
 $app = new \Slim\Slim();
 
+use \Hcode\Page;
 $app->config('debug', true);
 
 $app->get('/', function() {
-		$sql = new \Hcode\DB\Sql();
-		$results = $sql->select("select * from tb_users");
-	echo json_encode($results);
 
+ $page = new Page();
+  $page->setTpl("index");
 });
 
 $app->run();
